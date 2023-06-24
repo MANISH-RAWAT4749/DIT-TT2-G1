7de7c6f8d9d9c213abcbeacb1ca3e89805ea0af9
MANISH-RAWAT4749 <135990621+MANISH-RAWAT4749@users.noreply.github.com>
2023-06-24 15:54:46 +0530

    Create DNFProblemSolver.java

---
+++ ArrayQuestions/DNFProblemSolver.java
@@ -0,0 +1,36 @@
+public class DNFProblemSolver {
+    public static void sortArray(int[] arr) {
+        int low = 0;
+        int mid = 0;
+        int high = arr.length - 1;
+
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                swap(arr, low, mid);
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else if (arr[mid] == 2) {
+                swap(arr, mid, high);
+                high--;
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 0, 1, 1, 2, 0, 2, 1, 0};
+        sortArray(arr);
+
+        // Print the sorted array
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
