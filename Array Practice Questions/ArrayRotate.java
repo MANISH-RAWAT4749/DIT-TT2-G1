36e365f3871934555e2187be5edd0b08d03da460
MANISH-RAWAT4749 <135990621+MANISH-RAWAT4749@users.noreply.github.com>
2023-06-27 17:01:19 +0530

    Create ArrayRotate.java

---
+++ Array Practice Questions/ArrayRotate.java
@@ -0,0 +1,31 @@
+public class ArrayRotate {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+
+        rotate(nums, k);
+
+        System.out.println("Rotated Array:");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
