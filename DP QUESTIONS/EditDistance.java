b83628da5dda8ea6ba41e339ef57efb89766340a
MANISH-RAWAT4749 <135990621+MANISH-RAWAT4749@users.noreply.github.com>
2023-08-01 20:59:31 +0530

    Create EditDistance.java

---
+++ DP QUESTIONS/EditDistance.java
@@ -0,0 +1,37 @@
+public class EditDistance {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        // Filling the base cases
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+
+        // Filling the dp table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1]));
+                }
+            }
+        }
+
+        return dp[m][n]; // Minimum number of operations
+    }
+
+    public static void main(String[] args) {
+        EditDistance ed = new EditDistance();
+        String word1 = "horse"; // Replace this string with your first word
+        String word2 = "ros";   // Replace this string with your second word
+        System.out.println("Minimum number of operations: " + ed.minDistance(word1, word2));
+    }
+}
