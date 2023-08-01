572e51e5d475976fe6112745a42a65b3ae36bae6
MANISH-RAWAT4749 <135990621+MANISH-RAWAT4749@users.noreply.github.com>
2023-08-01 20:51:47 +0530

    Create ClimbingStairs.java

---
+++ DP/ClimbingStairs.java
@@ -0,0 +1,27 @@
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        ClimbingStairs cs = new ClimbingStairs();
+        int n = 5; // Replace 5 with the desired number of steps
+        System.out.println(cs.climbStairs(n));
+    }
+}
