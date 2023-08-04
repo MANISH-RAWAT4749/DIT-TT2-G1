be3e7b6814dd8176bbc17a28253eb63a006d6558
MANISH-RAWAT4749 <135990621+MANISH-RAWAT4749@users.noreply.github.com>
2023-08-04 21:32:48 +0530

    Create KnapProblem.java
    0-1 Knapsack Problem - Tabulation

---
+++ DP QUESTIONS 2/KnapProblem.java
@@ -0,0 +1,26 @@
+class KnapProblem {
+    static int knapSack(int W, int wt[], int val[], int n) {
+        int dp[][] = new int[n+1][W+1];
+        
+        for (int i = 0; i <= n; i++) {
+            for (int w = 0; w <= W; w++) {
+                if (i == 0 || w == 0)
+                    dp[i][w] = 0;
+                else if (wt[i-1] <= w)
+                    dp[i][w] = Math.max(val[i-1] + dp[i-1][w-wt[i-1]], dp[i-1][w]);
+                else
+                    dp[i][w] = dp[i-1][w];
+            }
+        }
+        
+        return dp[n][W];
+    }
+
+    public static void main(String args[]) {
+        int val[] = new int[] { 60, 100, 120 };
+        int wt[] = new int[] { 10, 20, 30 };
+        int W = 50;
+        int n = val.length;
+        System.out.println(knapSack(W, wt, val, n));
+    }
+}
